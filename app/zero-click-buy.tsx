257c18907081eb6603015bd60a398d4f8bb8e6ab
Rustam Goygov <rustam.goygov@coinbase.com>
2025-04-09 17:33:22 -0700

    OCB GC AP

--- app/zero-click-buy.tsx
+++ app/zero-click-buy.tsx
@@ -1,22 +1,37 @@
+import { LoadingScreen } from "@/components/LoadingScreen/LoadingScreen";
+import { ONRAMP_OCB_GC_AP_URL } from "@/constants/constants";
 import React from "react";
-import { StyleSheet } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { StyleSheet, View } from "react-native";
 import { WebView } from "react-native-webview";
 
 export default function ZeroClickBuyScreen() {
-  const insets = useSafeAreaInsets();
-  const placeholderUrl = "https://example.com"; // Replace with your actual URL
-
   return (
-    <WebView
-      style={[styles.container, { paddingTop: insets.top }]}
-      source={{ uri: placeholderUrl }}
-    />
+    <>
+      <WebView
+        style={styles.container}
+        source={{ uri: ONRAMP_OCB_GC_AP_URL }}
+        renderLoading={() => (
+          <View
+            style={{
+              flex: 1,
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            <LoadingScreen message="Loading..." />
+          </View>
+        )}
+        startInLoadingState={true}
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: "100%",
+    height: "100%",
   },
 });
--- constants/constants.ts
+++ constants/constants.ts
@@ -1,4 +1,3 @@
-import BaseIcon from "@/assets/icons/BaseIcon";
 import { OnrampPaymentMethod } from "./types";
 
 export const DEFAULT_ONRAMP_URL = "https://pay.coinbase.com";
@@ -6,8 +5,8 @@ export const DEFAULT_ONRAMP_URL = "https://pay.coinbase.com";
 /** The base URL for the Coinbase Onramp widget */
 export const ONRAMP_BUY_URL = `${DEFAULT_ONRAMP_URL}/buy`;
 
-export const ONRAMP_API_BASE_URL =
-  "https://api.developer.coinbase.com/onramp/v1";
+export const ONRAMP_OCB_GC_AP_URL =
+  "https://localhost:3000/v2/onramp/card-details?addresses=%7B%220x438BbEF3525eF1b0359160FD78AF9c1158485d87%22%3A%5B%22base%22%5D%7D&appId=36b7972f-b87f-4c13-a313-1b00db0212ec&assets=%5B%22ETH%22%5D&fiatCurrency=USD&presetFiatAmount=10";
 
 export const PRIVY_APP_ID = "cm79e5rbb05qoa6ziniztxby2";
 
@@ -17,106 +16,6 @@ export const PRIVY_CLIENT_ID =
 export const CDP_PROJECT_ID = "6eceb045-266a-4940-9d22-35952496ff00";
 
 export const CDP_CLIENT_API_KEY = "VmvIBxCiyNgDQ78njUIAql86kEokLMQ1";
-
-export const CURRENCY_OPTIONS = [
-  {
-    label: "USD",
-    value: "USD",
-    description: "US Dollar",
-    iconUrl: "https://wise.com/public-resources/assets/flags/rectangle/usd.png",
-    symbol: "$",
-  },
-  {
-    label: "EUR",
-    value: "EUR",
-    description: "Euro",
-    iconUrl: "https://wise.com/public-resources/assets/flags/rectangle/eur.png",
-    symbol: "€",
-  },
-  {
-    label: "GBP",
-    value: "GBP",
-    description: "British Pound",
-    iconUrl: "https://wise.com/public-resources/assets/flags/rectangle/gbp.png",
-    symbol: "£",
-  },
-  {
-    label: "JPY",
-    value: "JPY",
-    description: "Japanese Yen",
-    iconUrl: "https://wise.com/public-resources/assets/flags/rectangle/jpy.png",
-    symbol: "¥",
-  },
-  {
-    label: "CAD",
-    value: "CAD",
-    description: "Canadian Dollar",
-    iconUrl: "https://wise.com/public-resources/assets/flags/rectangle/cad.png",
-    symbol: "C$",
-  },
-];
-
-export const ASSET_OPTIONS = [
-  {
-    label: "ETH",
-    value: "ETH",
-    description: "ETH",
-    iconUrl: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
-  },
-  {
-    label: "USDC",
-    value: "USDC",
-    description: "USDC",
-    iconUrl: "https://cryptologos.cc/logos/usd-coin-usdc-logo.png",
-  },
-  {
-    label: "BTC",
-    value: "BTC",
-    description: "BTC",
-    iconUrl: "https://cryptologos.cc/logos/bitcoin-btc-logo.png",
-  },
-  {
-    label: "MATIC",
-    value: "MATIC",
-    description: "MATIC",
-    iconUrl: "https://cryptologos.cc/logos/polygon-matic-logo.png",
-  },
-  {
-    label: "SOL",
-    value: "SOL",
-    description: "SOL",
-    iconUrl: "https://cryptologos.cc/logos/solana-sol-logo.png",
-  },
-];
-
-// TODO: Get network options using onramp utils from the Onchainkit, once onchainkit is available in react native
-export const NETWORK_OPTIONS = [
-  {
-    label: "Base",
-    value: "base",
-    description: "Base",
-    iconUrl: undefined,
-    Icon: BaseIcon,
-  },
-  {
-    label: "Ethereum",
-    value: "ethereum",
-    description: "Ethereum",
-    iconUrl: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
-  },
-  {
-    label: "Optimism",
-    value: "optimism",
-    description: "Optimism",
-    iconUrl: "https://cryptologos.cc/logos/optimism-ethereum-op-logo.png",
-  },
-  {
-    label: "Solana",
-    value: "solana",
-    description: "Solana",
-    iconUrl: "https://cryptologos.cc/logos/solana-sol-logo.png",
-  },
-];
 
 export const PAYMENT_METHOD_OPTIONS: OnrampPaymentMethod[] = [
   {
